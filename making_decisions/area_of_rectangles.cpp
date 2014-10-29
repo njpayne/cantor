f46d66d66d6d7bfff5fff08ad8a869628c81e0e2
Nathaniel Payne <npayne@cardinalpath.com>
2014-10-29 11:52:54 -0700

    New program that compares aresa of different rectangles

--- making_decisions/area_of_rectangles.cpp
+++ making_decisions/area_of_rectangles.cpp
@@ -1,2 +1,46 @@
-# The area of a rectangle is its length times its width. Write a program that asks for the length and the width of the two rectangles
-# The program should then tell the user which rectangle has the greater area, or if the areas are the same?
+// The area of a rectangle is its length times its width. Write a program that asks for the length and the width of the two rectangles
+// The program should then tell the user which rectangle has the greater area, or if the areas are the same?
+
+# include <iostream>
+using namespace std;
+
+int main()
+{ // Create the main function
+	
+	// Instantiate the variables
+	double len1, len2, wid1, wid2, area1, area2;
+	
+	cout << "Welcome to the area comparison calculator!\n";
+	cout << "To use this calculator, enter the length and the width of the two different rectangles that you wish to compare!\n";
+	
+	// Get the length and width of the rectangles
+	cout << "Please enter the length of the first rectangle";
+	cin >> len1;
+	cout << "Please enter the width of the first rectangle";
+	cin >> wid1;
+	cout << "Please enter the length of the second rectangle";
+	cin >> len2;
+	cout << "Please enter the width of the second rectangle";
+	cin >> wid2;
+	
+	// Calcualte the area
+	area1 = len1 * wid1;
+	area2 = len2 * wid2;
+	
+	// Do the if comparison of the areas
+	if(area1 > area2)
+	{
+		cout << "Based on your inputs, rectangle 1 has a larger area than rectangle 2!\n";
+		cout << "In fact, the area of rectangle 1 is " << area1 << " and the area of rectangle 2 is " << area2;
+	}
+	else if(area1 < area2)
+	{
+		cout << "Based on your inputs, rectangle 2 has a larger area than rectangle 1!\n";
+		cout << "In fact, the area of rectangle 2 is " << area2 << " and the area of rectangle 2 is " << area1;
+	}
+	else // The two areas are equal
+	{
+		cout << "Based on your inputs, both of the rectangles have the same area!";
+	}
+
+}
--- making_decisions/max_min.cpp
+++ making_decisions/max_min.cpp
@@ -1,1 +1,25 @@
-# Write a program that takes in an array of numbers and returns the maximum and minimum of the numbers in the group
+// Write a program that takes in an array of numbers and returns the maximum and minimum of the numbers in the group
+
+# include <iostrea,>
+namespace std
+
+int main (){
+	// Create the main function
+	// Enter an arroy of numbers
+	
+	// Create the initial counter & other variables
+	int counter, maximum, minimum, number;
+	
+	// Create an empty array to hold the data
+	
+	// Input the array of numbers (count the number of inputs)
+	// This includes appending the array as the numbers are added
+	cout << "Welcome to the max / min number program! Please enter the numbers that you need into the model/n";
+		
+	// Do a comparison between all numbers to find the minimum and the maximum
+	
+	// Return the value of the number to the user
+	cout << "Thank you for using the min, max number program!\n";
+	cout << "The minimum of the array that you entered is " << minimum << "and the maximum of the number you entered is " << maximum;
+	
+}
