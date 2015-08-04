d09867cf81bbee681f69c3f6b23117f613476045
Nathaniel Payne <npayne@cardinalpath.com>
2015-08-03 22:15:10 -0700

    Adding the cantor base program

--- making_decisions/freeze_and_boil.cpp
+++ making_decisions/freeze_and_boil.cpp
@@ -1,6 +1,43 @@
 #The following table lists the freezing and boiling points of several substances. Write a program that asks the
-# user to enter a temperateur, and then shows all the substances that will freeze at that temperature and all
+# user to enter a temperature, and then shows all the substances that will freeze at that temperature and all
 # that will boil at that temperature. For example, if the user enters -20, the program should report that water will
 # freeze at that temperature and oxygen will boil at that temperature.
 # Ethyl alcohol: -173 Freezing; 172 boiling; Mercury: -38 freezing; 676 boiling; Oxygen: -362 freezing; -306 boiling
 # Water: 32 Freezing; 212 boiling
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Declare all relevant variables
+	double temperature;
+	
+	// Ask the user for input
+	cout << "Welcome to the temperature range information tool!\n";
+	cout << "This tool will tell you, depending on the temperature entered in F, \nthe substances that will boil or freeze at tha temperature!";
+	cout << "Please enter the temperature that you are currently thinking about!";
+	
+	cin >> temperature;
+	
+	// Evaluate the input and return the output to the user
+	if(temperature <= -362)
+	{
+		// Oxygen freezes
+	}
+	else if(temperature > -362 && temperature <= -306) ){
+		// Oxygen boils		
+	}
+	else if(temperature > -306 && temperature <= -173) ){
+		// Ethyl alcohol freezes	
+	}
+	else if(temperature > -173 && temperature <= -38) ){
+		// Mercury freezes		
+	}
+	else if(temperature > -38 && temperature <= 32) ){
+		// Water freezes		
+	}
+	else if(temperature > 32 && temperature <= 212) ){
+		// Oxygen boils		
+	}
+}
--- making_decisions/geometry_calculator.cpp
+++ making_decisions/geometry_calculator.cpp
@@ -5,13 +5,36 @@ Then, for the relative entry, be sure to calculate the correspondening corrected
 Be sure to deal with inappropriate entries */
 
 # include <iostream>
+//# include <boost/math/constants/constants.hpp> // Boost library for mathematical constants
+# include <cmath> // C math library
 using namespace std;
+//using namespace boost{ namespace math{ namespace constants{
+//
+//template <class T> T pi();
+//template <class T> T root_pi();
+//template <class T> T root_half_pi();
+//template <class T> T root_two_pi();
+//template <class T> T root_ln_four();
+//template <class T> T e();
+//template <class T> T half();
+//template <class T> T euler();
+//template <class T> T root_two();
+//template <class T> T ln_two();
+//template <class T> T ln_ln_two();
+//template <class T> T third();
+//template <class T> T twothirds();
+//template <class T> T pi_minus_three();
+//template <class T> T four_minus_pi();
+//
+//}}} // namespaces for boost
 
 int main()
 {
 	// Instantiate the program variables
 	int shape; // This will get the shape that the user will enter!
 	double radius, length, width, base, height, area;
+	const double pi = 3.1415;
+	//const double pi = boost::math::constants::pi<double>(); // For using pi
 	
 	// Get the users input
 	cout << "Welcome to the geometry calculator program!\n";
@@ -26,26 +49,35 @@ int main()
 		case 1: // Area of a circle	
 			cout << "\nEnter the radius of the circle: ";		
 			cin >> radius;
+			area = pi * pow(radius, 2);
+			cout << "The area of your circle is " << area;
 			break;
 		
-		case 1: // Area of a rectangle
+		case 2: // Area of a rectangle
 			cout << "\nEnter the length of the rectangle: ";	
 			cin >> length;
 			cout << "\nEnter the width of the rectangle: ";	
 			cin >> width;
+			area = length * width;
+			cout << "The area of your rectangle is " << area;
 			break;
 			
-		case 1: // Area of a triangle
+		case 3: // Area of a triangle
 			cout << "\nEnter the base of the triangle: ";	
 			cin >> base;
 			cout << "\nEnter the height of the triangle: ";	
 			cin >> height;
+			area = (base * height)/2;
+			cout << "The area of your triangle is " << area;
 			break;
 		
-		case 1: // Quit
+		case 4: // Quit
+			cout << "I am sorry that you are choosing to leave the program and ignore math! Good riddance!";
 			break;
 			
 		default:
 			// If the user enters a value that is not in the original set
+			cout << "We are sorry, but you entered a choice that doesn't make any sense! Learn to read!";
 	}
+	return 0;
 }
